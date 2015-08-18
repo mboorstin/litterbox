8833cbe12c802eae509b1a08a631f58b1011e188
Marcus Boorstin <mindstormsguy@verizon.net>
2015-08-18 12:03:40 -0700

    Updated ultrasound

--- hub/ultrasound_proto.py
+++ hub/ultrasound_proto.py
@@ -5,6 +5,8 @@ from urlparse import urlparse
 from httplib import HTTPConnection
 import json
 
+THRESHOLD = 15
+
 url = "http://litterbox.moistcake.net/api/v1.0/stalls"
 # Not that the secret is being used right now...
 headers = {"Content-Type": "application/json"}
@@ -14,9 +16,21 @@ conn = HTTPConnection(urlparts.netloc, urlparts.port or 80)
 print "Connecting to Xbee..."
 xbee = serial.Serial("/dev/ttyUSB0", 9600)
 print "Connected to Xbee..."
+old_status = None
 while True:
-    xbee_data = xbee.read()
-    print "Data: ", xbee_data
+    data = xbee.readline()
+    try:
+        distance = int(data)
+    except:
+        print "Unable to parse data: ", data
+        continue
 
-    data = json.dumps({'secret': '1234567890', 'stall_id': 0, 'status': True})
-    conn.request("POST", urlparts.path, data, headers)
+    print distance
+    status = distance >= 0 and distance < THRESHOLD
+    
+    if status != old_status:
+        print "Status changed to", status
+        data = json.dumps({'secret': '1234567890', 'stall_id': 0, 'status': True})
+        # conn.request("POST", urlparts.path, data, headers)
+
+    old_status = status
