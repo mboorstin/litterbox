8ff30e1992732d820bd073efb4417fd8a153057a
Jonathan Lee <jonlee@jonlee-mbp.corp.dropbox.com>
2015-08-20 16:40:34 -0700

    better error messages and stats

--- server/project/app.py
+++ server/project/app.py
@@ -17,6 +17,10 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite+pysqlite:///sqlite.db'
 db = SQLAlchemy(app)
 Scss(app, asset_dir='assets/scss', static_dir='static/css')
 
+stats = {
+    'longest_poop': 0,
+}
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -33,16 +37,19 @@ def get_stalls():
 
 @app.route('/api/v1.0/stalls', methods=['POST'])
 def update_stalls():
+    data = request.get_data(as_text=True)
     required = ['secret', 'stall_id', 'status']
-    if not request.json or any(x not in request.json for x in required):
-        abort(400)
+    if not request.json:
+        abort(400, 'request must be in application/json format')
+    if any(x not in request.json for x in required):
+        abort(400, 'request must contain %s' % (', '.join(required),))
     if request.json['secret'] != SECRET:
-        abort(400)
+        abort(400, 'secret is incorrect')
 
     session = db.session()
     stall = session.query(Stall).get(request.json['stall_id'])
     if not stall:
-        abort(400)
+        abort(400, 'no stall found')
     if stall.status and not request.json['status']:
         stall.visits.order_by(Visit.id.desc()).first().exited_at = datetime.datetime.now()
         stall.status = request.json['status']
@@ -58,8 +65,10 @@ def update_stalls():
 @app.route('/api/v1.5/stalls', methods=['POST'])
 def update_stalls_from_raw():
     required = ['raw_data']
-    if not request.json or any(x not in request.json for x in required):
-        abort(400)
+    if not request.json:
+        abort(400, 'request must be in application/json format')
+    if any(x not in request.json for x in required):
+        abort(400, 'request must contain %s' % (', '.join(required),))
     message_str = base64.b64decode(request.json['raw_data'])
     message_bytes = bytearray()
     message_bytes.extend(message_str)
@@ -71,10 +80,12 @@ def update_stalls_from_raw():
     session = db.session()
     stall = session.query(Stall).filter_by(address=address).first()
     if not stall:
-        abort(400)
+        abort(400, 'no stall found')
     if stall.status and not status:
-        stall.visits.order_by(Visit.id.desc()).first().exited_at = datetime.datetime.now()
         stall.status = False
+        visit = stall.visits.order_by(Visit.id.desc()).first()
+        visit.exited_at = datetime.datetime.now()
+        stats['longest_poop'] = max(visit.duration, stats['longest_poop'])
         session.commit()
     elif not stall.status and status:
         session.add(Visit(stall_id=stall.id))
@@ -88,18 +99,23 @@ def get_visits(stall_id):
     try:
         stall_id = int(stall_id)
     except ValueError:
-        abort(400)
+        abort(400, 'no such stall')
 
     session = db.session()
     stall = session.query(Stall).get(stall_id)
     if not stall:
-        abort(400)
+        abort(400, 'no such stall')
 
     response = make_response()
     visits = stall.visits.order_by(Visit.id.desc()).all()
     values = [visit.to_json() for visit in visits]
     response.data = json.dumps(values)
     return response
+
+@app.route('/api/v1.5/stats', methods=['GET'])
+def get_stats():
+    return jsonify(stats), 200
+
 
 @app.route('/api/v1.0/debug', methods=['GET'])
 @app.route('/api/v1.5/debug', methods=['GET'])
